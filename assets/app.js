3a928f43152f8c4519c474d4b5591cf8f73c1229
axon7 <kubaz9991@gmail.com>
2021-01-22 21:27:26 +0100

    User entity and repository

--- assets/app.js
+++ assets/app.js
@@ -7,7 +7,7 @@
 
 // any CSS you import will output into a single css file (app.css in this case)
 import './styles/app.scss';
-import './styles/show.scss'
+// import './styles/show.scss'
 
 // start the Stimulus application
 import './bootstrap';
