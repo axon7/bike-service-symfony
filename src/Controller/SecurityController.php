4c3e2ad77cf85ee9f9d0c3a6b86dd714d65e5746
axon7 <kubaz9991@gmail.com>
2021-03-02 00:09:59 +0100

    Register user

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -2,10 +2,12 @@
 
 namespace App\Controller;
 
+use App\Form\UserRegistrationFormType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 
@@ -30,6 +32,30 @@ class SecurityController extends AbstractController
             'controller_name' => 'SecurityController',
             'last_username' => $lastUsername,
             'error' => $error,
+        ]);
+    }
+
+    /**
+     * @Route ("/register", name="app_register")
+     * @param Request $request
+     * @param UserPasswordEncoderInterface $passwordEncoder
+     * @return Response
+     */
+    public function register(Request $request, UserPasswordEncoderInterface $passwordEncoder): Response
+    {
+        $form = $this->createForm(UserRegistrationFormType::class);
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid()){
+            $user = $form->getData();
+            $user->setPassword($passwordEncoder->encodePassword($user, $user->getPassword()));
+
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($user);
+            $entityManager->flush();
+        }
+
+        return $this->render('register/index.html.twig', [
+            'registrationForm' => $form->createView()
         ]);
     }
 
